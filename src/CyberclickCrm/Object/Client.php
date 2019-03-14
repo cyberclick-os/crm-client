9b1530dda0bc262261bf2b5af7345f2043a6cca5
Héctor Borrás <hborras@cyberclick.net>
2019-03-14 15:05:17 +0100

    Añadir client

--- src/CyberclickCrm/Object/Client.php
+++ src/CyberclickCrm/Object/Client.php
@@ -4,7 +4,52 @@
 namespace CyberclickCrm\Object;
 
 
-class Client
-{
+use CyberclickCrm\Api;
+use CyberclickCrm\ApiRequest;
+use CyberclickCrm\Http\RequestInterface;
+use CyberclickCrm\Object\Fields\ClientFields;
+use CyberclickCrm\TypeChecker;
 
+class Client extends AbstractObject
+{
+    /** @var integer */
+    private $id;
+
+    public function __construct($id = '', Api $api = null)
+    {
+        parent::__construct($api);
+
+        $this->id = $id;
+    }
+
+    public static function getFieldsEnum()
+    {
+        return ClientFields::getInstance();
+    }
+
+    public function getStats($params)
+    {
+        $params['client'] = $this->id;
+
+        $paramTypes = [
+        ];
+
+        $enums = [
+        ];
+
+        $request = new ApiRequest(
+            $this->api,
+            null,
+            RequestInterface::METHOD_GET,
+            'stats',
+            new Stats(),
+            null,
+            null,
+            new TypeChecker($paramTypes, $enums)
+        );
+
+        $request->addParams(['params' => $params]);
+
+        return $request->execute();
+    }
 }
---
+++ src/CyberclickCrm/Object/Fields/ClientFields.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CyberclickCrm\Object\Fields;
+
+use CyberclickCrm\Enum\AbstractEnum;
+
+class ClientFields extends AbstractEnum
+{
+    const ID = 'id';
+}
